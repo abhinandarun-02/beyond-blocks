15d7f65945fde6f6678809429dfb7267e8a45666
Abhinand Arun <abhinandarun2015@gmail.com>
2023-12-09 16:51:16 +0530

    Add backend

---
+++ api/api-routes.js
@@ -0,0 +1,16 @@
+import {Router} from "express";
+const router= Router();
+let info;
+router.get('/',function(req,res){
+    res.send("Hello World");
+})
+router.post('/create',function(req,res){
+    const data= req.body;
+    info=data.inputMsg;
+    res.status(201).json({"Success":info});
+    
+})
+router.get('/read',function(req,res){
+    res.json(info);
+})
+export default router;
---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express,{json} from "express";
+import cors from "cors";
+
+import logger from "morgan";
+
+import apiroutes from "./api-routes.js";
+
+const listen_port=8000;
+
+const app=express();
+
+app.use(cors({origin:"*"}));
+app.use(json());
+app.use(logger('dev'));
+
+app.use('/',apiroutes);
+
+app.listen(listen_port,()=>{
+    console.log(`Listening to port${listen_port}`)
+})
